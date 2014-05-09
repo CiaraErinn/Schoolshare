bab8978deea32112f851f773f068586fcdd2bbc2
Ciara Watson <ciara_erinn@hotmail.com>
2014-05-09 18:39:06 +0200

    Created a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Schoolshare::Application.config.secret_key_base = '5f76de28027c57c0c3bbd85a43114802233430159e98ecec14b55f6a8774354f757c6370e94a5c09cd130a6e20888bccbd52c810313b806a1aa5785ece603a8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Schoolshare::Application.config.session_store :cookie_store, key: '_schoolshare_session'
